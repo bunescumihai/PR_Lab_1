8ba5951e5cf89d1a5d13c80a43058215838015c2
Bunescu Mihai <bunescumihai1958@gmail.com>
2024-02-11 00:42:30 +0200

    Lists of Threads and Connections

--- Server/Program.cs
+++ Server/Program.cs
@@ -8,6 +8,9 @@ IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
 int port = 9000;
 bool run = true;
 
+List <Socket> connections = new List <Socket>();
+List <Thread> threads = new List <Thread>();
+
 try
 {
     serverSocket.Bind(new IPEndPoint(ipAddress, port));
@@ -32,55 +35,104 @@ if (run)
 
 while (run)
 {
+    Thread controlConnections = new Thread(new ThreadStart(ControlConnections));
+    Thread controlThreads = new Thread(new ThreadStart(ControlThreads));
+    controlConnections.Start();
+    controlThreads.Start();
+
     Socket connection = serverSocket.Accept();
-    Console.Write("Accepted a connection");
-    ThreadStart threadStart = new ThreadStart(() => ServeConnection(connection));
-    Thread thread = new Thread(threadStart);
-    thread.Start();
-    
-}
+    connections.Add(connection);
 
-void ServeConnection(Socket connection)
-{
     clientsConnected++;
-
-    int clientNumber = clientsConnected;
 
     Console.WriteLine("A client has connected");
     Console.WriteLine($"Total: {clientsConnected} connections");
 
+}
+
+void ServeConnection(Socket connection)
+{
+
     StringBuilder sb = new StringBuilder();
 
-    while (connection.Connected)
+    do
     {
-        do
-        {
-            byte[] buffer = new byte[1024];
-
-            try
-            {
-                int bytesReceived = connection.Receive(buffer);
-                sb.Append(Encoding.UTF8.GetString(buffer, 0, bytesReceived));
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-
-        } while (connection.Available > 0);
+        byte[] buffer = new byte[1024];
 
         try
         {
-            connection.Send(Encoding.UTF8.GetBytes("Server receive your message"));
+            int bytesReceived = connection.Receive(buffer);
+            sb.Append(Encoding.UTF8.GetString(buffer, 0, bytesReceived));
         }
-        catch (Exception ex) { }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
 
-        Console.WriteLine(sb.ToString());
-        sb = sb.Clear();
+    } while (connection.Available > 0);
 
+    try
+    {
+        connection.Send(Encoding.UTF8.GetBytes("Server receive your message"));
     }
+    catch { }
 
-    clientsConnected--;
-    Console.WriteLine("A client has deconnected");
-    Console.WriteLine($"Total: {clientsConnected} connections");
+    Console.WriteLine(sb.ToString());
+    sb = sb.Clear();
+
+}
+
+void ControlConnections()
+{
+    while (true)
+    {
+        Thread.Sleep(100);
+        try
+        {
+            foreach (Socket conn in connections)
+            {
+                if (!conn.Connected)
+                {
+                    clientsConnected--;
+
+                    Console.WriteLine("A client has deconnected");
+                    Console.WriteLine($"Total: {clientsConnected} connections");
+
+                    connections.Remove(conn);
+
+                    continue;
+                }
+
+                if (conn.Available > 0)
+                {
+                    while (threads.Count > 10)
+                        Thread.Sleep(50);
+
+                    Thread thread = new Thread(new ThreadStart(() => ServeConnection(conn)));
+                    threads.Add(thread);
+                    thread.Start();
+
+                }
+            }
+        }
+        catch { }
+    }
+}
+
+
+void ControlThreads()
+{
+    while (true)
+    {
+        Thread.Sleep(100);
+        try
+        {
+            foreach (Thread th in threads)
+            {
+                if (!th.IsAlive)
+                    threads.Remove(th);
+            }
+        }
+        catch { }
+    }
 }
